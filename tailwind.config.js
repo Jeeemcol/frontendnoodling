0f061570b120e948654a960b1e25cf50f93d0267
Jim <->
2023-10-30 20:41:10 +0000

    corrected purge/ content (purge no longer used in config--content is instead

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false,
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
